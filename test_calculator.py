aca3914e6a2afd9eee68f38a694d902e6713525c
yanarusko23-create <yana.rusko.23@pnu.edu.ua>
2026-05-19 07:41:04 +0300

    Fix PEP8 formatting issues

--- test_calculator.py
+++ test_calculator.py
@@ -3,8 +3,10 @@ from calculator import add, subtract
 
 
 class TestCalculator(unittest.TestCase):
+
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
 
